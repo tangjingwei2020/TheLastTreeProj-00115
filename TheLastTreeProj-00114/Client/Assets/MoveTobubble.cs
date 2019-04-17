867221f274a16dd14b5675eec7483d8d7b3de9f4
tangjingwei88 <jingweitang88@gmail.com>
2019-04-17 23:02:01 +0800

    dd

---
+++ TheLastTreeProj-00114/Client/Assets/MoveTobubble.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTobubble : MonoBehaviour {
+
+    float speed = 0.1f;
+    float timer = 0;
+    GameObject obj;
+	// Use this for initialization
+	void Start () {
+        obj = GameObject.FindGameObjectWithTag("tree");
+	}
+
+
+    void Update()
+    {
+        if (obj != null)
+        {
+            float step = speed * Time.deltaTime;
+      
+            this.gameObject.SetActive(true);
+            this.gameObject.transform.localPosition = new Vector3(Mathf.Lerp(this.gameObject.transform.localPosition.x, obj.transform.localPosition.x, step), Mathf.Lerp(this.gameObject.transform.localPosition.y, obj.transform.localPosition.y, step), Mathf.Lerp(this.gameObject.transform.localPosition.z, obj.transform.localPosition.z, step));//插值算法也可以
+ 
+        }
+    }
+
+
+}
